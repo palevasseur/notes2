32e01a5c6c057b269acb843fc08e579a874e2b91
Paul-Antoine <palevasseur@gmail.com>
2017-06-15 23:13:11 +0200

    format link in text component

--- src/app/dynamicComponents/text.component.ts
+++ src/app/dynamicComponents/text.component.ts
@@ -13,24 +13,35 @@ export class TextComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.textContent.nativeElement.innerHTML = this.text;
+    let text = this.text;
+
+    text = this.escapeHtml(text);
+
+    // html link (https://regex101.com/r/vYEDZz/2)
+    text = text.replace(/(?:\[(.*?)\])?\(?(https?:\/\/[^ \n\r()]+)\)?/gi, (match, $1, $2) => {
+      return $1
+        ? '<a href="' + $2 + '" title="' + $2 + '" >' + $1 + '</a>'
+        : '<a href="' + $2 + '">' + $2 + '</a>';
+    });
+
+    this.textContent.nativeElement.innerHTML = text;
   }
 
-  /*escapeHtml(text) {
-    return text.replace(/[&<>"'\/{}]/g, function (s) {
+  escapeHtml(text) {
+    return text.replace(/[&<>"'{}]/g, function (s) {
       var entityMap = {
         '&': '&amp;',
         '<': '&lt;',
         '>': '&gt;',
         '"': '&quot;',
         '\'': '&#39;',
-        '/': '&#x2F;',
+//        '/': '&#x2F;',
         '{': '&#123;',
         '}': '&#125;'
       };
 
       return entityMap[s];
     });
-  }*/
+  }
 
 }
--- src/app/notes-app/format-text.pipe.ts
+++ src/app/notes-app/format-text.pipe.ts
@@ -2,16 +2,8 @@ import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({name: 'formatText'})
 export class FormatTextPipe implements PipeTransform {
-  private static noteId = 1;
 
   transform(text: string) {
-    // html link (https://regex101.com/r/vYEDZz/2)
-    text = text.replace(/(?:\[(.*?)\])?\(?(https?:\/\/[^ \n\r()]+)\)?/gi, (match, $1, $2) => {
-      return $1
-        ? '<a href="' + $2 + '" title="' + $2 + '" >' + $1 + '</a>'
-        : '<a href="' + $2 + '">' + $2 + '</a>';
-    });
-
     // plantuml tag => uml diagram
     text = text.replace(/@startuml((?:.|\n|\r)*?)@enduml/gi, (match, $1) => {
       return '<app-uml code="' + this.escapeQuote($1) + '" diagram="plantuml"></app-uml>';
@@ -30,7 +22,6 @@ export class FormatTextPipe implements PipeTransform {
     // text class tag
     text = text.replace(/@text((?:.|\n|\r)*?)@text/gi, (match, $1) => {
       return '<app-text text="' + this.escapeQuote($1) + '"></app-text>';
-      //return '<div style="white-space: pre-wrap;">'+this.escapeHtml($1)+'</div>'; // "pre-wrap" style to manage space, tab and \n
     });
 
 
